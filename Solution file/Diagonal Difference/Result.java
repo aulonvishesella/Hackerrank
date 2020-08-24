174af690170bd563db352a408cbaac16453239b3
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-24 20:00:35 +0100

    Update Result.java

--- Solution file/Diagonal Difference/Result.java
+++ Solution file/Diagonal Difference/Result.java
@@ -19,21 +19,21 @@ class Result {
     public static int diagonalDifference(List<List<Integer>> arr) {
 
    
-    int aCol= 0;
-    int bCol = 0;
-    int j1 =0;
-    int j2=arr.size()-1;
-    for(int i=0;i<arr.size();i++){
-        aCol += arr.get(i).get(j1);
-        j1++;
-    }
-    for(int i=0;i<arr.size();i++){
-        bCol += arr.get(i).get(j2);
-        j2--;
-    }
+        int aCol= 0;
+        int bCol = 0;
+        int j1 =0;
+        int j2=arr.size()-1;
+        for(int i=0;i<arr.size();i++){
+            aCol += arr.get(i).get(j1);
+            j1++;
+        }
+        for(int i=0;i<arr.size();i++){
+            bCol += arr.get(i).get(j2);
+            j2--;
+        }
 
-    return Math.abs(aCol - bCol); 
+        return Math.abs(aCol - bCol); 
 
-    }
+        }
 
 }
