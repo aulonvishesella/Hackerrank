fa6e2a24de8495494fa22353723fb2ce51f9cbf8
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-24 19:53:26 +0100

    Add files via upload

---
+++ Solution file/Plus Minus/Solution.java
@@ -0,0 +1,41 @@
+package Solution;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        
+        float pNum = 0;
+        float nNum = 0;
+        float zNum = 0;
+        //format to 5 decimal places
+        DecimalFormat df = new DecimalFormat("#.######");
+ 
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                nNum+=1;
+            }
+            else if(arr[i]>0){
+                pNum += 1;
+            }
+            else{
+                zNum+=1;
+            }
+        }
+
+        
+
+        System.out.println(df.format(pNum/arr.length));
+        System.out.println(df.format(nNum/arr.length));
+        System.out.println(df.format(zNum/arr.length));
+
+    }
+}
