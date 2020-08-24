b393557206a8d728670cb21a3a09994ba6f2e36a
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-24 19:07:13 +0100

    Add files via upload

---
+++ sjsjsjsj/Solution.java
@@ -0,0 +1,20 @@
+package Solution;
+
+	import java.io.*;
+	import java.util.*;
+	import java.text.*;
+	import java.math.*;
+	import java.util.regex.*;
+
+	public class Solution {
+
+
+
+	    static int solveMeFirst(int a, int b) {
+	        return a+b;
+	      
+		}
+
+	
+	}
+
