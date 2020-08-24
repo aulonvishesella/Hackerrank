983d59aba7a0c9ae686e72fc299d9dbcb3ffaa84
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-24 19:26:04 +0100

    Add files via upload

---
+++ Solution file/Compare The Triplets/Solution.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+    	
+            int aScore = 0;
+            int bScore = 0;
+            List<Integer> scores = new ArrayList<Integer>(); 
+            for(int i =0 ; i<3;i++){
+                if(a.get(i) > b.get(i)){
+                    aScore +=1;
+                }
+                else if(a.get(i) < b.get(i)){
+                    bScore+=1;
+                }
+                else{
+                    aScore +=0;
+                    bScore +=0;
+                }
+            }
+             scores.add(aScore);
+             scores.add(bScore);
+            return scores;
+
+           
+    }
+}
+
