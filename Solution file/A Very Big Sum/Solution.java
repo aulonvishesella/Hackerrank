052524dcc3d74fe1019c9a479931277350768b00
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-25 19:35:06 +0100

    Update Solution.java

--- Solution file/A Very Big Sum/Solution.java
+++ Solution file/A Very Big Sum/Solution.java
@@ -1,9 +1,11 @@
 package Solution;
 
 public class Solution {
+  
   static long aVeryBigSum(long[] ar) {
             long bigSum = 0;
             for(int i = 0; i<ar.length;i++){
+                //add the current array value with bigSum
                 bigSum += ar[i];
             }
             return bigSum;
