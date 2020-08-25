3426b386808394cc6015d96e310f42bc62188eab
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-25 18:39:55 +0100

    Update Solution.java

--- Solution file/Electronic Shop/Solution.java
+++ Solution file/Electronic Shop/Solution.java
@@ -1,5 +1,33 @@
 package Solution;
 
 public class Solution {
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+     
+        int maxExpenditure = 0;
+        //for each keyboard, we loop through each drive
+        for(int i=0;i<keyboards.length;i++){
+            for(int j=0;j<drives.length;j++){
+            	//if both the sum keyboard and drive does not exceed limit
+                if(keyboards[i]+drives[j]<=b){
+                	//if the sum exceeds the current max, then we set the current max to this new sum
+                    if(keyboards[i]+drives[j]>maxExpenditure){
+                        maxExpenditure = keyboards[i]+drives[j];
+                    }
+                }
+            }
+        }
+        
+        //if max does not change, it means we the drives and keyboard combo exceeded limit
+        if(maxExpenditure == 0){
+            return -1;
+        }
+        else {
+            return maxExpenditure;
+        }
 
+
+    }
 }
