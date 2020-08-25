c98ddfd4d2eb5f74ae6b80e43fcc04c36f8f2c5e
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-08-25 18:28:49 +0100

    Add files via upload

---
+++ Solution file/Divisible Sum Pair/Solution.java
@@ -0,0 +1,21 @@
+package Solution;
+
+public class Solution {
+	 
+	static int divisibleSumPairs(int n, int k, int[] ar) {
+         //n= length of ar
+         //k = what we need to divide it by
+         //ar = array of the integers
+         int counter=0;
+         for(int i=0;i<n;i++){
+             for(int j=i+1;j<n;j++){
+                 if((ar[i] + ar[j])% k == 0){
+                     counter+=1;
+                 }
+             }
+         }
+
+         return counter;
+       
+	}
+}
