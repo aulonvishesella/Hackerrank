7f7588824768a57232392523a419895b5e2d9cbb
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-09-01 15:36:04 +0100

    Update Result.java

--- Solution file/Picking Numbers/Result.java
+++ Solution file/Picking Numbers/Result.java
@@ -10,32 +10,26 @@ import java.util.regex.*;
 
 class Result {
 
-    /*
-     * Complete the 'pickingNumbers' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY a as parameter.
-     */
-
+    //o(n^2) time complexity
     public static int pickingNumbers(List<Integer> a) {
-    // Write your code here
-   int temp =0;
-   int count =0;
-    for(int i=0;i<a.size();i++){
-         for(int j=0; j<a.size();j++){
-             if(a.get(i) - a.get(j)==1 || a.get(i) - a.get(j) == 0 ){
-                 temp++;
+        // Write your code here
+       int temp =0;
+       int count =0;
+        for(int i=0;i<a.size();i++){
+             for(int j=0; j<a.size();j++){
+                 if(a.get(i) - a.get(j)==1 || a.get(i) - a.get(j) == 0 ){
+                     temp++;
+                 }
              }
-         }
-         if(temp> count){
-             count = temp;
-             temp=0;
-         }
-         else{
-             temp=0;
-         }  
-    } 
-    return count;
-    }
+             if(temp> count){
+                 count = temp;
+                 temp=0;
+             }
+             else{
+                 temp=0;
+             }  
+        } 
+        return count;
+        }
 
 }
