407dd169bc7f7d5da675bc056f44e4173c7f255d
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-09-01 12:00:04 +0100

    Add files via upload

---
+++ Solution file/Cat And Mouse/Solution.java
@@ -0,0 +1,27 @@
+package Solution;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    // Complete the catAndMouse function below.
+    static String catAndMouse(int x, int y, int z) {
+
+        if(Math.abs(z-x) < Math.abs(z-y)){
+            return "Cat A";
+        }
+        else if(Math.abs(z-x) > Math.abs(z-y)){
+            return "Cat B";
+        }
+        else{
+            return "Mouse C";
+        }
+
+    }
+}
