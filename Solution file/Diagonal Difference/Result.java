efc04a54fc360b11af90a9e58cb7dfa7c252e187
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-09-01 15:29:32 +0100

    Update Result.java

--- Solution file/Diagonal Difference/Result.java
+++ Solution file/Diagonal Difference/Result.java
@@ -9,13 +9,7 @@ import java.util.regex.*;
 
 class Result {
 
-    /*
-     * Complete the 'diagonalDifference' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-     */
-
+    //O(n) time complexity
     public static int diagonalDifference(List<List<Integer>> arr) {
 
    
