23dca17725bdaa260c2e8aa3d9f90ff3f711d267
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-09-04 12:47:30 +0100

    Update Solution.java

--- Solution file/Lisa's Workbook/Solution.java
+++ Solution file/Lisa's Workbook/Solution.java
@@ -1,5 +1,36 @@
 package Solution;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
+//o(n^2) time complexity
 public class Solution {
 
-}
+    // Complete the workbook function below.
+    static int workbook(int n, int k, int[] arr) {
+        int count = 0;
+        int currentPage = 1;
+        // chapter
+        for(int i = 0; i < arr.length; i++) {
+            int NumberOfProblems = arr[i];
+            // problems
+            for(int j = 1; j <= arr[i]; j++) {
+                //if the current problem == current page number, then add to count
+                if(j == currentPage) {
+                    count++;
+                }
+                // the current problem has reached max number for given page, then 
+                //increase page
+                if((j % k == 0) || j == NumberOfProblems) {
+                    currentPage++;
+                }
+            }
+        }
+        return count;
+    }
+
+  
