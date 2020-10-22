d9c345f0c8ad30f71e607076e882c9c0048902da
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-10-22 14:23:55 +0100

    Update Solution.java

--- Solution file/SockMerchant/Solution.java
+++ Solution file/SockMerchant/Solution.java
@@ -6,9 +6,11 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+
+//O(n) time complexity
 public class Solution {
 
-    // Complete the sockMerchant function below.
+    // create a hashmap where the key is the integer and the value is the count
     static int sockMerchant(int n, int[] ar) {
         HashMap<Integer, Integer> colours = new HashMap<Integer, Integer>();
         int countPairs=0;
@@ -16,6 +18,7 @@ public class Solution {
             if(!colours.containsKey(ar[i])){
                 colours.put(ar[i],1);
             }else{
+                //if we there are 2 of these so far, we count the number of pairs and remove the key from the hashmap
                 countPairs+=1;
                 colours.remove(ar[i]);
             }
