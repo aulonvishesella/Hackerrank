77fd15d80a1193c5baf07d1a1dda77f8b8f190cd
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-10-26 17:58:22 +0000

    Add files via upload

---
+++ Solution file/Reverse Linked List/Solution.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the reverse function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+
+     //O(n) time complexity worst case, O(1) time complexity best case
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        DoublyLinkedListNode trackedHead =head;
+        DoublyLinkedListNode current=head;
+        //if list is empty
+        if(current==null){
+            return null;
+        }
+        //if only 1 value
+        else if(current.next == null){
+            return current;
+        }
+        else{
+             while(current!=null){
+                 //make a temp node of the current previous
+                 DoublyLinkedListNode prev = current.prev;
+                 //current previous reference will reference not next to current
+                 current.prev=current.next;
+                 //current next reference will reference the temp prev node
+                 current.next=prev;
+                 //update the headtracker
+                 trackedHead=current;
+                 //set current to be the node previous to it
+                 current=current.prev;
+                
+            }
+        }
+        return trackedHead;
