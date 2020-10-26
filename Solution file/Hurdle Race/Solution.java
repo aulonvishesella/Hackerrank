c46275e4bd2ad03518fbdd1f08cb3ef16bb5178e
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-10-26 12:11:25 +0000

    Update Solution.java

--- Solution file/Hurdle Race/Solution.java
+++ Solution file/Hurdle Race/Solution.java
@@ -1,4 +1,29 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
+//O(n) time complexity
 public class Solution {
+    static int hurdleRace(int k, int[] height) {
+        //find the max unit character can jump
+        int max = height[0];
+        for(int i=0;i<height.length;i++){
+            if(max<height[i]){
+                max=height[i];
+            }
+        }
+        //can already jump all the hurdles, so no need to drink anymore doses
+        if(max<=k){
+            return 0;
+        }
+        //character can jump unit of 'max', but the tallest hurdle has a height of k
+        //therefore need max-k number of doses
+        else{
+            return max-k;
+        }
 
-}
+    }
