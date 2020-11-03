e4f1013600375f7aa429f958de27fc2f31ef40b3
Aulon <37848112+aulonvishesella@users.noreply.github.com>
2020-11-03 20:20:19 +0000

    Update Solution.java

--- Solution file/Making Anagrams/Solution.java
+++ Solution file/Making Anagrams/Solution.java
@@ -6,13 +6,16 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
 
-    // Complete the makeAnagram function below.
+//O(n) time complexity
+public class Solution {
+    
     static int makeAnagram(String first, String second) {  
         
+        //hashmap of the characters with the occurence of the character in the strings
         Map<Character, Integer> characterCount = new HashMap<>();
         
+        //loop through the first string. If the current character is not in the map, add it with 1, else we increment the value count of the character
         for(int i=0;i<first.length();i++){
             char c = first.charAt(i);
             
@@ -26,6 +29,7 @@ public class Solution {
 
         }
          
+         //loop through the second string. If the current character is not in the map, add 0, else we decrement the value count of the character
         for(int i=0;i<second.length();i++){
             char c = second.charAt(i);
             
@@ -38,7 +42,9 @@ public class Solution {
             }
 
         }
-     
+        
+        //loop through each value from all values in the map. Add the values to count. (if there is an equal pair, the no need to add anything as there is an anagram.
+        //else, we have to add the value to the count
         int count = 0;
         for( Integer value : characterCount.values()) {
             if(value!=0){
